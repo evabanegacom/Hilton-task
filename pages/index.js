6caffa7e188a81f94d4f98fc46b3870e7c22ec7c
Precious chiedu <udegbue69@gmail.com>
2021-11-05 10:42:55 +0100

    made responsiveness for all screens

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,11 @@ export default function Home() {
         @media only screen and (max-width: 767px) {
             display: flex;
             justify-content: space-between;
+
+            .px-2 {
+                padding-right: 0;
+                padding-left: 1.5em;
+            }
         }
     `;
 
@@ -161,7 +166,7 @@ export default function Home() {
                                 d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                             />
                         </svg>
-                        <span>EN (S)</span>
+                        <span>EN&nbsp;(S)</span>
                     </div>
                 </div>
             </SmallItems>
