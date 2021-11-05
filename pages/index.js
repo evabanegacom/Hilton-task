5a9df82627e510c2ce979398e29c4fba0209711b
Precious chiedu <udegbue69@gmail.com>
2021-11-05 11:18:36 +0100

    adjusted view height for iphone 5

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,7 @@ export default function Home() {
         @media only screen and (max-width: 768px) {
             width: 100%;
             margin: 30px 0 0;
+            margin-top: 50px;
         }
 
         @media only screen and (min-width: 769px) and (max-width: 1024px) {
